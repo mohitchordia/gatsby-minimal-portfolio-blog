fc347d2d91d883fc6dddc9ec5bd9f561f2c51667
mohitchordia <mohitchordia07@gmail.com>
2020-05-16 18:06:07 +0530

    Updated INtro

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,10 +25,10 @@ I'm Mohit Chordia - Software Engineer Who likes to build new stuff.<br/><br/>
 I have confidence in my ability as a Engineer and a attitude to give 100% in my tasks , with the belief in life long approach
 of learning and growing myself, As a Engineer. <br/>
 <br/>
-Progaming Languges that I have knowledge of are C++, Java, Kotlin, Python.
-As a Android Developer I have experience using Android SDK, Firebase, JSON, RESTful APIs, XML, Material Design, Dagger, Android Testing, Retrofit, RxJava, MLkit
-In field of Machine Learning things i have hands on experience are Python, Numpy, Pandas, MatplotLib, SciKitLearn, Tensorflow, OpenCV.
-Apart from this i have used Java to build Application on top of both Spring Core and Spring MVC.<br/>
+Progaming Languges that I have knowledge of are C++, Java, Kotlin, Python.<br/>
+As a Android Developer I have experience using Android SDK, Firebase, JSON, RESTful APIs, XML, Material Design, Dagger, Android Testing, Retrofit, RxJava, MLkit
+<br/>In field of Machine Learning things i have hands on experience are Python, Numpy, Pandas, MatplotLib, SciKitLearn, Tensorflow, OpenCV.
+<br/>Apart from this i have used Java to build Application on top of both Spring Core and Spring MVC.<br/>
 <br/>
 Ready to work with people who want to be as successful(according to Their defination) as I wanna be.
 So that we as people can help each other grow and reach our destination. 
